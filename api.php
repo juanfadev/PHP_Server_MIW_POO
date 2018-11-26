9e7e9043e1ec65c5387f9caeffddb77524496a76
juanfpo96 <juanfpo96@gmail.com>
2018-11-26 18:37:18 +0100

    CORS

--- api.php
+++ api.php
@@ -9,6 +9,7 @@
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
+header("Access-Control-Allow-Origin: *");
 $input = json_decode(file_get_contents('php://input'), true);
 
 // retrieve the entity and key from the path
